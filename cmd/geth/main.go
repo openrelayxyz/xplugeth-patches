f961d9850fb3ac0990f1859a8ab466bfc90be278
Philip Morlier <philipmorlier@gmail.com>
2024-11-07 12:15:54 -0800

    xplugeth/hooks/initialize foundation patch
    This patch adds the necessary hooks to support xplugeth/hooks/initialize dependent plugins on foundation geth.

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -39,6 +39,12 @@ import (
 	"github.com/ethereum/go-ethereum/node"
 	"go.uber.org/automaxprocs/maxprocs"
 
+	//begin xplugeth injection
+	"path/filepath"
+	"github.com/openrelayxyz/xplugeth"
+	//end xplugeth injection
+
+
 	// Force-load the tracer engines to trigger registration
 	_ "github.com/ethereum/go-ethereum/eth/tracers/js"
 	_ "github.com/ethereum/go-ethereum/eth/tracers/live"
@@ -54,6 +60,10 @@ const (
 var (
 	// flags that configure the node
 	nodeFlags = flags.Merge([]cli.Flag{
+		//begin xplugeth injection
+		utils.DisablePluginsFlag,
+		utils.PluginsConfigDirFlag,
+		//end xplugeth injection
 		utils.IdentityFlag,
 		utils.UnlockedAccountFlag,
 		utils.PasswordFileFlag,
@@ -338,6 +348,18 @@ func prepare(ctx *cli.Context) {
 // It creates a default node based on the command line arguments and runs it in
 // blocking mode, waiting for it to be shut down.
 func geth(ctx *cli.Context) error {
+	//begin xplugeth injection
+	if !ctx.IsSet(utils.DisablePluginsFlag.Name) {
+		var pluginsConfigDir string
+		if ctx.IsSet(utils.PluginsConfigDirFlag.Name) {
+			pluginsConfigDir = ctx.String(utils.PluginsConfigDirFlag.Name)
+		} else {
+			pluginsConfigDir = filepath.Join(ctx.String(utils.DataDirFlag.Name), "pluginsconfig")
+		}
+		xplugeth.Initialize(pluginsConfigDir)
+		log.Info("xplugeth initialized")
+	}
+	//end xplugeth injection
 	if args := ctx.Args().Slice(); len(args) > 0 {
 		return fmt.Errorf("invalid command: %q", args[0])
 	}
@@ -346,7 +368,19 @@ func geth(ctx *cli.Context) error {
 	stack := makeFullNode(ctx)
 	defer stack.Close()
 
+	//begin xplugeth injection
+	xplugeth.StoreSingleton[*node.Node](stack)
+	stack.RegisterAPIs(pluginGetAPIs())
+	//end xplugeth injection
+	
 	startNode(ctx, stack, false)
+	
+	//begin xplugeth injection
+	pluginInitializeNode()
+	pluginBlockchain()
+	defer pluginOnShutdown()
+	//end xplugeth injection
+
 	stack.Wait()
 	return nil
 }
