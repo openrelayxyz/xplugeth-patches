8bf60ed0624851d21b94fb67c2ddec4a7ff56d91
Philip Morlier <philipmorlier@gmail.com>
2025-02-22 18:50:02 -0800

    xplugeth/hooks/stateupdates foundation patch
    This patch adds the necessary hooks to support xplugeth/hooks/stateupdates dependent plugins on foundation geth.

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -1256,6 +1256,10 @@ func (s *StateDB) commit(deleteEmptyObjects bool, noStorageWiping bool) (*stateU
 	origin := s.originalRoot
 	s.originalRoot = root
 
+	//begin xplugeth code injection
+	writeToRootMap(root, deletes)
+	//end xplugeth injection
+
 	return newStateUpdate(noStorageWiping, origin, root, deletes, updates, nodes), nil
 }
 
@@ -1280,6 +1284,9 @@ func (s *StateDB) commitAndFlush(block uint64, deleteEmptyObjects bool, noStorag
 		// If snapshotting is enabled, update the snapshot tree with this new version
 		if snap := s.db.Snapshot(); snap != nil && snap.Snapshot(ret.originRoot) != nil {
 			start := time.Now()
+			//begin xplugeth code injection
+			pluginStateUpdate(ret.root, ret.originRoot, ret.accounts, ret.storages, ret.codes)
+			//end xplugeth injection
 			if err := snap.Update(ret.root, ret.originRoot, ret.accounts, ret.storages); err != nil {
 				log.Warn("Failed to update snapshot tree", "from", ret.originRoot, "to", ret.root, "err", err)
 			}
