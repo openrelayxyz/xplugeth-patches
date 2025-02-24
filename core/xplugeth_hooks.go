20867e93eb9f48d18e22c47b8e36bd545b9763cb
Philip Morlier <philipmorlier@gmail.com>
2025-02-24 12:39:21 -0800

    xplugeth/hooks/blockchain foundation patch
    This patch adds the necessary hooks to support xplugeth/hooks/blockchain dependent plugins on foundation geth.

---
+++ core/xplugeth_hooks.go
@@ -0,0 +1,58 @@
+package core
+
+import (
+	"math/big"
+	"sync"
+	"time"
+
+	"github.com/openrelayxyz/xplugeth"
+	"github.com/openrelayxyz/xplugeth/utils"
+	"github.com/openrelayxyz/xplugeth/hooks/blockchain"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/log"
+)
+
+func pluginNewHead(block *types.Block, hash common.Hash, logs []*types.Log) {
+	td, err := utils.GetTd(hash)
+	if err != nil {
+		log.Error("error returned from call to GetTD, pluginNewHead", "err", err)
+		td = big.NewInt(0)
+	}
+	for _, m := range xplugeth.GetModules[blockchain.NewHeadPlugin]() {
+		m.NewHead(block, hash, logs, td)
+	}
+}
+
+func pluginNewSideBlock(block *types.Block, hash common.Hash, logs []*types.Log) {
+	for _, m := range xplugeth.GetModules[blockchain.NewSideBlockPlugin]() {
+		m.NewSideBlock(block, hash, logs)
+	}
+}
+
+func pluginReorg(commonBlock *types.Header, oldChain, newChain []*types.Header) {
+	oldChainHashes := make([]common.Hash, len(oldChain))
+	for i, block := range oldChain {
+		oldChainHashes[i] = block.Hash()
+	}
+	newChainHashes := make([]common.Hash, len(newChain))
+	for i, block := range newChain {
+		newChainHashes[i] = block.Hash()
+	}
+	for _, m := range xplugeth.GetModules[blockchain.ReorgPlugin]() {
+		m.Reorg(commonBlock.Hash(), oldChainHashes, newChainHashes)
+	}
+}
+
+func pluginSetTrieFlushIntervalClone(flushInterval time.Duration) time.Duration {
+	m := xplugeth.GetModules[blockchain.SetTrieFlushIntervalClonePlugin]()
+	var snc sync.Once
+	if len(m) > 1 {
+		snc.Do(func() { log.Warn("The blockChain flushInterval value is being accessed by multiple plugins") })
+	}
+	for _, m := range m {
+		flushInterval = m.SetTrieFlushIntervalClone(flushInterval)
+	}
+	return flushInterval
+}
